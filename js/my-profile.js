293163b1b94c622dbeb8de0ec6cb53a915c15ad8
Mai <maielissalde1gmail.com>
2020-11-08 22:55:27 -0300

    Entrega 7
    Creación de espacio perfil y cambios en forma de mostrar productos

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,80 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+// funcion que corrobora que los datos introducidos sean correctos y si lo son, llama a funcion guardar
+function corroborar() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let edad = document.getElementById("edad").value;
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
 
-});
+    if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
+        alert("Debe completar el nombre")
+        return false;
+    }
+
+    else if (apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)) {
+        alert("Debe completar el apellido")
+        return false;
+    }
+
+    else if (telefono.length < 9 || /^\s+$/.test(telefono)) {
+        alert("Debe ingresar un número de teléfono válido")
+        return false;
+    }
+
+    else if (edad == null||edad < 16 || /^\s+$/.test(edad)) {
+        alert("Debe ser mayor de 16 para tener una cuenta")
+        return false;
+    }
+
+    else if (!/\S+@\S+\.\S+/.test(correo)) {
+        alert("Debe introducir un correo valido")
+        return false;
+    }
+    
+
+    guardarDatos()
+}
+
+
+// función que accede a los valores de los inputs y crea objeto para guardarlo. Luego pasa a string y guarda en local Storage
+function guardarDatos() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let edad = document.getElementById("edad").value;
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+
+
+    let objeto = {
+        nombreObj: nombre,
+        apellidoObj: apellido,
+        edadObj: edad,
+        telefonoObj: telefono,
+        correoObj: correo,
+    }
+
+    let objetoString = JSON.stringify(objeto);
+    localStorage.setItem("objetoUsuario", objetoString);
+
+}
+
+
+// tomo los datos del localStore y los muestro dentro de los inputs
+document.addEventListener("DOMContentLoaded", function (e) {
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let edad = document.getElementById("edad");
+    let telefono = document.getElementById("telefono");
+    let correo = document.getElementById("correo");
+    
+
+    let objetoFinal = JSON.parse(localStorage.getItem("objetoUsuario"));
+
+
+    nombre.value = objetoFinal.nombreObj
+    apellido.value = objetoFinal.apellidoObj
+    edad.value = objetoFinal.edadObj
+    telefono.value = objetoFinal.telefonoObj
+    correo.value = objetoFinal.correoObj
+
+})
--- js/products.js
+++ js/products.js
@@ -1,18 +1,23 @@
 const ascendente = "0aMax";
 const descendente = "maxA0";
 const relevancia = "rel";
+let lista = []
 var ordenados = [];
 let criterio = [];
 let min = undefined
 let max = undefined
 
+// trae el array con la información y la envía a ordenar de forma ascendente
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(productos =>{
         if (productos.status === "ok")
-        {sortProducts(ascendente, productos.data);}
+        { lista = productos.data
+        sortProducts(ascendente, lista);
+        }
     });
 });
 
+// Ordena el array según costo y cantidad de vendidos y envía a filtrar
 function sortProducts(criterio, array){
      if (criterio === ascendente){
         ordenados = array.sort((a, b) => {
@@ -28,6 +33,7 @@ function sortProducts(criterio, array){
     filtro(ordenados)
 }
 
+// Filtra según precio y envía a mostrar en HTML
 function filtro(){
     let filtrados = [];
     
@@ -39,6 +45,7 @@ function filtro(){
  showProductsList(filtrados)
 }
 
+// Muestra la información en el HTML
 function showProductsList(array){
 
     let html = "";
@@ -46,28 +53,24 @@ function showProductsList(array){
         let product = array[i];
 
         html += `
-        <a href="product-info.html" class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ product.name +`</h4>
-                        <small class="text-muted">` + product.soldCount + ` productos vendidos</small>
-                    </div>
-                    <p>` + product.currency + ` ` + product.cost + `</p>
-                    <p class= "mb-1"> ` + product.description +`</p>
-                    <button type="button" class="btn btn-primary" onclick="window.location.href='product-info.html'">Ver más información</button>
-                </div>
+        <div class="col-4">
+        <a href="product-info.html" class="card mb-4 shadow-sm custom-card">
+            <img class="bd-placeholder-img card-img-top" src="${product.imgSrc}">
+             <h3 class="m-3">${product.name} </h3>
+            <div class="card-body">
+            <p class="card-text">${product.description}</p>
             </div>
+            <h4 class="m-3 text-right">${product.currency + " " + product.cost}</h4>
+            <h5 class="m-3 text-right text-muted">${product.soldCount} Unidades vendidas</h5>
         </a>
+        </div>
         `
 
         document.getElementById("lista").innerHTML = html;
     }
 }
 
+// Según dónde se hace click determinan un criterio y llaman a función de ordenar
 document.getElementById("ordAsc").addEventListener("click", function(){
     criterio = ascendente
     sortProducts(criterio, lista);
@@ -91,14 +94,21 @@ document.getElementById("clearFilter").addEventListener("click", function(){
 
 document.getElementById("filterCount").addEventListener("click", function(){
 
-    minP = document.getElementById("countMin").value;
-    maxP = document.getElementById("countMax").value;
+minP = document.getElementById("countMin").value;
+maxP = document.getElementById("countMax").value;
 
-    if ((minP) >= 0){min = minP} 
-    else {min = 0;}
+if ((minP) >= 0){
+    min = minP
+} else {
+    min = 0;
+}
 
-    if ((maxP) >= 0){max = maxP} else {
-    max = 400000;}
+if ((maxP) >= 0){
+    max = maxP
+} else {
+    max = 400000;
+}
 
-    sortProducts(criterio, lista);
+sortProducts(criterio, lista);
 });
+
